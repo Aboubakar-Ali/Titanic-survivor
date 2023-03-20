38f8d2b7807012494bb33fd54befffddd3352b01
Aboubakar-Ali <93763546+Aboubakar-Ali@users.noreply.github.com>
2023-03-20 16:08:38 +0100

    Titanic

--- files/app.py
+++ files/app.py
@@ -1,12 +1,10 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request, jsonify, render_template, send_from_directory
 from flask_cors import CORS
-import joblib  
+import joblib
+import os
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='../static', template_folder='../templetes')
 CORS(app)
-
-# Charger le modèle entraîné
-model = joblib.load('logistic_regression_model.pkl')
 
 # Charger le modèle entraîné
 model = joblib.load('logistic_regression_model.pkl')
@@ -30,6 +28,26 @@ def predict():
     response = {'survived': int(prediction[0])}
 
     return jsonify(response)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predict_page')
+def predict_page():
+    return render_template('predict.html')
+
+@app.route('/visualisations')
+def visualisations():
+    return render_template('visualisation.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/static/<path:path>')
+def send_static(path):
+    return send_from_directory('../static', path)
 
 if __name__ == '__main__':
     app.run(debug=True)
